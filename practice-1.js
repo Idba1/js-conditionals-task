becdf52fee79cc015d4a8e8b5ddb8c2151ee976c
Idba1 <moniraislamself@gmail.com>
2024-01-27 04:09:24 +0600

    practice task -1

---
+++ practice-1.js
@@ -0,0 +1,25 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+
+*/
+
+let burgerPrice = 770;
+if(burgerPrice > 500){
+    console.log('free Coke');
+}
+else{
+    console.log('you can buy coke 30tk');
+}
+
+
+
+let burgerPrice1 = 470;
+if(burgerPrice1 > 500){
+    console.log('free Coke');
+}
+else{
+    console.log('you can buy coke 30tk');
+}
