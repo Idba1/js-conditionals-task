fd6abbc0df950513f48cb8943092eab9e75f9cf3
Idba1 <moniraislamself@gmail.com>
2024-01-27 05:27:44 +0600

    practice problem -4

---
+++ practice-4.js
@@ -0,0 +1,46 @@
+/***
+
+if you get more then 80 then inside your friend score. 
+    If your friend get more than 80. then go for a lunch. 
+    if your friend get below 80 but greater than or equal 60 then tell your friend, good luck next time. 
+    if your friend get less than 60 but more than or equal to 40 then, keep your friend's message unseen.
+    if your friend get less than 40, block your friend
+if you get less than 80 go to home and sleep and act sad
+
+Note: 
+use nested if-else-if-else
+*/
+
+
+let myMarks = 87;
+let myFriendMarks = 68;
+
+
+
+if(myMarks < 80){
+    console.log('go to home and sleep and act sad');
+}
+else{
+    console.log('give your self 1 treat');
+}
+
+
+
+
+if(myFriendMarks > 80){
+    console.log('go for a lunch.');
+}
+else{
+    if( myFriendMarks >= 60){  //myFriendMarks < 80 ||
+        console.log('good luck next time.');
+    }
+    else{
+        if(myFriendMarks >= 40){  //myFriendMarks < 60 || 
+            console.log('keep your friends message unseen.');
+        }
+
+            else{
+                console.log('block your friend')
+            }
+    }
+}
